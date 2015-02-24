5b6250c2e5909c210f3c65a64ca6cab063a994f0
ivossss <ivossss@yahoo.com>
2015-02-24 15:02:30 +0200

    C# part 2 exam. Task 4 is done.

--- C#/C# part 2/10 Exam/04. VariableLengthCodes/VariableLengthCodes.cs
+++ C#/C# part 2/10 Exam/04. VariableLengthCodes/VariableLengthCodes.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace _04.VariableLengthCodes
 {
@@ -11,29 +10,49 @@ namespace _04.VariableLengthCodes
         static void Main()
         {
             string firstLine = Console.ReadLine();
-            string[] sequenceOfIntegersAsEncodedText = firstLine.Split(' ');
-            int numberOfLines = int.Parse(Console.ReadLine());
-            string[] codeTable = new string[numberOfLines];
+            string[] sequenceOfIntegersAsEncodedText = firstLine.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            char[] codeTable = ReadCodeTable();
+            string binaryCode = DecodeFromIntToBinary(sequenceOfIntegersAsEncodedText);
+            string decodedText = Decode(codeTable, binaryCode);
+            Console.WriteLine(decodedText);
+        }
 
-            for (int i = 0; i < codeTable.Length; i++)
+        static char[] ReadCodeTable()
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            char[] codeTable = new char[numberOfLines + 1];
+            string currentRow;
+            char currentSymbol;
+            int currentSymbolBitsCount;
+
+            for (int i = 0; i < numberOfLines; i++)
             {
-                codeTable[i] = Console.ReadLine();
+                currentRow = Console.ReadLine();
+                currentSymbol = currentRow[0];
+                currentSymbolBitsCount = int.Parse(currentRow.Substring(1));
+                codeTable[currentSymbolBitsCount] = currentSymbol;
             }
 
+            return codeTable;
+        }
+
+        static string DecodeFromIntToBinary(string[] sequenceOfIntegers)
+        {
             StringBuilder fullBinaryCode = new StringBuilder();
             int currentInteger;
             string currentBinaryNumber;
             string fullCurrentBinaryNumber;
 
-            for (int i = 0; i < sequenceOfIntegersAsEncodedText.Length; i++)
+            for (int i = 0; i < sequenceOfIntegers.Length; i++)
             {
-                currentInteger = int.Parse(sequenceOfIntegersAsEncodedText[i]);
+                currentInteger = int.Parse(sequenceOfIntegers[i]);
                 currentBinaryNumber = Convert.ToString(currentInteger, 2);
                 fullCurrentBinaryNumber = AddMissingZerosAtTheBeggining(currentBinaryNumber);
                 fullBinaryCode.Append(fullCurrentBinaryNumber);
             }
 
             string finalBinaryCode = RemoveZerosAtTheEnd(fullBinaryCode);
+            return finalBinaryCode;
         }
 
         static string RemoveZerosAtTheEnd(StringBuilder binaryCode)
@@ -61,6 +80,27 @@ namespace _04.VariableLengthCodes
             }
 
             return result.ToString();
+        }
+
+        static string Decode(char[] codeTable, string binaryCode)
+        {
+            int counter = 0;
+            StringBuilder decodedText = new StringBuilder();
+
+            for (int i = 0; i < binaryCode.Length; i++)
+            {
+                if (binaryCode[i] == '1')
+                {
+                    counter++;
+                }
+                if (binaryCode[i] == '0' || i == binaryCode.Length - 1)
+                {
+                    decodedText.Append(codeTable[counter]);
+                    counter = 0;
+                }
+            }
+
+            return decodedText.ToString();
         }
     }
 }
