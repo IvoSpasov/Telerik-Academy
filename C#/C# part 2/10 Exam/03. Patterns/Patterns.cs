d0ff291081f30abcc5c3ca5d9f2f93c0416c52f5
ivossss <ivossss@yahoo.com>
2015-02-24 11:31:52 +0200

    C@ part 2 exam. Task 3 is 100% done.

--- C#/C# part 2/10 Exam/03. Patterns/Patterns.cs
+++ C#/C# part 2/10 Exam/03. Patterns/Patterns.cs
@@ -1,47 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _03.Patterns
+﻿namespace _03.Patterns
 {
+    using System;
+
     class Patterns
     {
         static void Main()
         {
             int[,] matrix = ReadInputMatrix();
-            int sum = 0;
-            int bestSum = 0;
-            int A, B, C, D, F, E, G;
+            int bestSum = CalculateBestSum(matrix);
 
-            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            if (bestSum != int.MinValue)
             {
-                for (int col = 0; col < matrix.GetLength(1) - 4; col++)
-                {
-                    A = matrix[row, col];
-                    B = matrix[row, col + 1];
-                    C = matrix[row, col + 2];
-                    D = matrix[row + 1, col + 2];
-                    F = matrix[row + 2, col + 2];
-                    E = matrix[row + 2, col + 3];
-                    G = matrix[row + 2, col + 4];
-
-                    bool areConsecutive = (A == B - 1) && (B == C - 1) && (C == D - 1) && (D == F - 1) && (F == E - 1) && (E == G - 1);
-
-                    if (areConsecutive)
-                    {
-                        sum = A + B + C + D + F + E + G;
-                    }
-                }
-
-                if (bestSum < sum)
-                {
-                    bestSum = sum;
-                }
+                Console.WriteLine("YES " + bestSum);
             }
-
-            Console.WriteLine("YES " + bestSum);
+            else
+            {
+                bestSum = CalculateSumOnMainDiagonal(matrix);
+                Console.WriteLine("NO " + bestSum);
+            }
         }
 
         static int[,] ReadInputMatrix()
@@ -63,6 +39,53 @@ namespace _03.Patterns
             }
 
             return matrix;
+        }
+
+        static int CalculateBestSum(int[,] matrix)
+        {
+            int sum = int.MinValue;
+            int bestSum = int.MinValue;
+            int A, B, C, D, F, E, G;
+            bool areConsecutive;
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 4; col++)
+                {
+                    A = matrix[row, col];
+                    B = matrix[row, col + 1];
+                    C = matrix[row, col + 2];
+                    D = matrix[row + 1, col + 2];
+                    F = matrix[row + 2, col + 2];
+                    E = matrix[row + 2, col + 3];
+                    G = matrix[row + 2, col + 4];
+
+                    areConsecutive = (A == B - 1) && (B == C - 1) && (C == D - 1) && (D == F - 1) && (F == E - 1) && (E == G - 1);
+
+                    if (areConsecutive)
+                    {
+                        sum = A + B + C + D + F + E + G;
+                    }
+                    if (bestSum < sum)
+                    {
+                        bestSum = sum;
+                    }
+                }
+            }
+
+            return bestSum;
+        }
+
+        static int CalculateSumOnMainDiagonal(int[,] matrix)
+        {
+            int sum = 0;
+
+            for (int row = 0, col = 0; row < matrix.GetLength(0); row++, col++)
+            {
+                sum += matrix[row, col];
+            }
+
+            return sum;
         }
     }
 }
