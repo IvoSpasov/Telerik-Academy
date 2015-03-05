de689474ea08b62f038b69b9f24e4627abe11a2a
ivossss <ivossss@yahoo.com>
2015-03-05 16:37:30 +0200

    C# OOP fractions practice.

---
+++ C#/C# OOP/OOP-Training/OOP-Training/Fraction.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Training
+{
+    class Fraction
+    {
+        private int numerator;
+
+        private int denumerator;
+
+        public Fraction(string fraction)
+        {
+            this.Parse(fraction);
+        }
+
+        public decimal DecimalValue
+        {
+            get { return (decimal)this.numerator / this.denumerator; }
+        }
+
+        private void Parse(string fraction)
+        {
+            if (!fraction.Contains('/'))
+            {
+                throw new InvalidCastException("The fraction is not valid because it does not contain \"/\" ");
+            }
+
+            string[] splittedFraction = fraction.Split('/');
+            int num = int.Parse(splittedFraction[0]);
+            int denum = int.Parse(splittedFraction[1]);
+
+            if (num == 0)
+            {
+                throw new InvalidOperationException("The numerator must be different than 0");
+            }
+            if (denum == 0)
+            {
+                throw new InvalidOperationException("The denumerator must be different than 0");
+            }
+
+            this.numerator = num;
+            this.denumerator = denum;
+        }
+
+        public void CancelFraction()
+        {
+            if (this.numerator == default(int))
+            {
+                throw new InvalidOperationException("The numerator must be different than 0");
+            }
+            if (this.denumerator == default(int))
+            {
+                throw new InvalidOperationException("The denumerator must be different than 0");
+            }
+
+            int num = this.numerator;
+            int denum = this.denumerator;
+            int resultFromDivision;
+            int reminderFromDivision = num % denum;
+
+            while (reminderFromDivision != 0)
+            {
+                resultFromDivision = num / denum;
+                reminderFromDivision = num % denum;
+                num = denum;
+                denum = reminderFromDivision;
+            }
+
+            this.numerator /= num;
+            this.denumerator /= num;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", this.numerator, this.denumerator);
+        }
+    }
+}
---
+++ C#/C# OOP/OOP-Training/OOP-Training/FractionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Training
+{
+    class FractionTest
+    {
+        static void Main(string[] args)
+        {
+            Fraction fract = new Fraction("18/48");
+            Console.WriteLine(fract);
+            Console.WriteLine(fract.DecimalValue);
+            fract.CancelFraction();
+            Console.WriteLine(fract);
+        }
+    }
+}
