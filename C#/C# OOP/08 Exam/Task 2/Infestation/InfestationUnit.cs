87288fe66420b3fe512a8a00c442b9b67c9ffe3f
ivossss <ivossss@yahoo.com>
2015-03-15 20:35:50 +0200

    Queen unit and an InfestationUnit abstract class added

---
+++ C#/C# OOP/08 Exam/Task 2/Infestation/InfestationUnit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infestation
+{
+    public abstract class InfestationUnit : Unit
+    {
+        public InfestationUnit(string id, UnitClassification unitType, int health, int power, int aggression)
+            : base(id, unitType, health, power, aggression)
+        {
+        }
+
+        protected override bool CanAttackUnit(UnitInfo unit)
+        {
+            bool attackUnit = false;
+            if (this.Id != unit.Id)
+            {
+                attackUnit = true;
+            }
+
+            return attackUnit;
+        }
+
+        protected override UnitInfo GetOptimalAttackableUnit(IEnumerable<UnitInfo> attackableUnits)
+        {
+            UnitInfo optimalAttackableUnit = new UnitInfo(null, UnitClassification.Unknown, int.MaxValue, 0, 0);
+
+            foreach (var unit in attackableUnits)
+            {
+                if (unit.Health < optimalAttackableUnit.Health)
+                {
+                    optimalAttackableUnit = unit;
+                }
+            }
+
+            return optimalAttackableUnit;
+        }
+    }
+}
--- C#/C# OOP/08 Exam/Task 2/Infestation/Parasite.cs
+++ C#/C# OOP/08 Exam/Task 2/Infestation/Parasite.cs
@@ -2,7 +2,7 @@
 {
     using System.Collections.Generic;
 
-    public class Parasite : Unit
+    public class Parasite : InfestationUnit
     {
         private const int ParasiteHealth = 1;
         private const int ParasitePower = 1;
@@ -13,30 +13,6 @@
         {
         }
 
-        protected override bool CanAttackUnit(UnitInfo unit)
-        {
-            bool attackUnit = false;
-            if (this.Id != unit.Id)
-            {
-                attackUnit = true;
-            }
 
-            return attackUnit;
-        }
-
-        protected override UnitInfo GetOptimalAttackableUnit(IEnumerable<UnitInfo> attackableUnits)
-        {
-            UnitInfo optimalAttackableUnit = new UnitInfo(null, UnitClassification.Unknown, int.MaxValue, 0, 0);
-
-            foreach (var unit in attackableUnits)
-            {
-                if (unit.Health < optimalAttackableUnit.Health)
-                {
-                    optimalAttackableUnit = unit;
-                }
-            }
-
-            return optimalAttackableUnit;
-        }
     }
 }
