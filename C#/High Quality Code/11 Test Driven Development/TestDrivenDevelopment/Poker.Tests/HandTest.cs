45d5141f7a82f1fce2be145268f537a8f0c78985
ivossss <ivossss@yahoo.com>
2015-03-19 13:44:16 +0200

    Hand functionality and tests added

--- C#/High Quality Code/11 Test Driven Development/TestDrivenDevelopment/Poker.Tests/HandTest.cs
+++ C#/High Quality Code/11 Test Driven Development/TestDrivenDevelopment/Poker.Tests/HandTest.cs
@@ -1,10 +1,11 @@
 ﻿namespace Poker.Tests
 {
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System;
     using System.Collections.Generic;
 
     using Interfaces;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;   
 
     [TestClass]
     public class HandTest
@@ -12,15 +13,100 @@
         [TestMethod]
         public void TestHandToSting()
         {
-            IHand hand = new Hand(new List<ICard>() { 
+            var cards = new List<ICard>() 
+            { 
                 new Card(CardFace.Ace, CardSuit.Clubs),
                 new Card(CardFace.Ace, CardSuit.Diamonds),
                 new Card(CardFace.King, CardSuit.Hearts),
                 new Card(CardFace.King, CardSuit.Spades),
-                new Card(CardFace.Seven, CardSuit.Diamonds),
-            });
+                new Card(CardFace.Seven, CardSuit.Diamonds)
+            };
+
+            var hand = new Hand(cards);
 
             Assert.AreEqual("A♣, A♦, K♥, K♠, 7♦", hand.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestEmptyHandToString()
+        {
+            var cards = new List<ICard>();
+            var hand = new Hand(cards);
+            hand.ToString();
+        }
+
+        [TestMethod]
+        public void TestAddCardToHand()
+        {
+            var hand = new Hand();
+            var card = new Card(CardFace.Jack, CardSuit.Hearts);
+            hand.AddCards(card);
+            Assert.AreEqual("J♥", hand.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestAddNullCardToHand()
+        {
+            var hand = new Hand();
+            Card card = null;
+            hand.AddCards(card);
+        }
+
+        [TestMethod]
+        public void TestAddCardsToHand()
+        {
+            var firstCards = new List<ICard>() 
+            { 
+                new Card(CardFace.Ace, CardSuit.Clubs),
+                new Card(CardFace.Ace, CardSuit.Diamonds),
+                new Card(CardFace.King, CardSuit.Hearts)
+            };
+
+            var secondCards = new List<ICard>()
+            {                
+                new Card(CardFace.King, CardSuit.Spades),
+                new Card(CardFace.Seven, CardSuit.Diamonds)
+            };
+
+            var hand = new Hand(firstCards);
+            hand.AddCards(secondCards);
+            Assert.AreEqual("A♣, A♦, K♥, K♠, 7♦", hand.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestAddNullListListOfCardsToHand()
+        {
+            var firstCards = new List<ICard>() 
+            { 
+                new Card(CardFace.Ace, CardSuit.Clubs),
+                new Card(CardFace.Ace, CardSuit.Diamonds),
+                new Card(CardFace.King, CardSuit.Hearts)
+            };
+
+            List<ICard> secondCards = null;
+
+            var hand = new Hand(firstCards);
+            hand.AddCards(secondCards);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAddEmptyListOfCardsToHand()
+        {
+            var firstCards = new List<ICard>() 
+            { 
+                new Card(CardFace.Ace, CardSuit.Clubs),
+                new Card(CardFace.Ace, CardSuit.Diamonds),
+                new Card(CardFace.King, CardSuit.Hearts)
+            };
+
+            var secondCards = new List<ICard>();
+
+            var hand = new Hand(firstCards);
+            hand.AddCards(secondCards);
         }
     }
 }
--- C#/High Quality Code/11 Test Driven Development/TestDrivenDevelopment/Poker/Hand.cs
+++ C#/High Quality Code/11 Test Driven Development/TestDrivenDevelopment/Poker/Hand.cs
@@ -2,30 +2,58 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Text;
+    using System.Linq;
 
     using Interfaces;
 
     public class Hand : IHand
     {
-        public IList<ICard> Cards { get; private set; }
+        public Hand()
+        {
+            this.Cards = new List<ICard>();
+        }
 
         public Hand(IList<ICard> cards)
         {
             this.Cards = cards;
         }
 
-        public override string ToString()
+        public IList<ICard> Cards { get; private set; }
+
+        public void AddCards(ICard card)
         {
-            StringBuilder hand = new StringBuilder();
-            foreach (var card in this.Cards)
+            if (card == null)
             {
-                hand.Append(card.ToString());
-                hand.Append(", ");
+                throw new ArgumentNullException("card", "THe card cannot be null");
             }
 
-            hand.Remove(hand.Length - 2, 2);
-            return hand.ToString();
+            this.Cards.Add(card);
+        }
+
+        public void AddCards(IList<ICard> cards)
+        {
+            if (cards == null)
+            {
+                throw new ArgumentNullException("cards", "The cards list cannot be null");
+            }
+
+            if (cards.Count == 0)
+            {
+                throw new ArgumentException("The list of cards is empty");
+            }
+
+            this.Cards = this.Cards.Concat(cards).ToList();
+        }
+
+        public override string ToString()
+        {
+            if (this.Cards.Count == 0)
+            {
+                throw new InvalidOperationException("Cannot make a string from an empty list");
+            }
+
+            string result = string.Join(", ", this.Cards);
+            return result;
         }
     }
 }
