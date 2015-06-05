bba38cafa85d06698ba29b30b5184c86f51f0db1
ivossss <ivossss@yahoo.com>
2015-06-05 12:40:48 +0300

    JS Data types and variables refactoring

--- JavaScript/JavaScript Fundamentals/01 Data Types and Variables/All_Tasks.js
+++ JavaScript/JavaScript Fundamentals/01 Data Types and Variables/All_Tasks.js
@@ -23,21 +23,21 @@
     // Task 3
     // Try typeof on all variables you created.
 
-    console.log(number + ' is from type ' + typeof (number));
-    console.log(floatingNumber + ' is from type ' + typeof (floatingNumber));
-    console.log(string + ' is from type ' + typeof (string));
+    console.log(number + ' is from type ' + typeof number);
+    console.log(floatingNumber + ' is from type ' + typeof floatingNumber);
+    console.log(string + ' is from type ' + typeof string);
     console.log(bool + ' is from type ' + typeof (bool));
-    console.log(objectVariable + ' is from type ' + typeof (objectVariable));
-    console.log(notANumber + ' is from type ' + typeof (notANumber));
-    console.log(func.name + ' is from type ' + typeof (func));
+    console.log(objectVariable + ' is from type ' + typeof objectVariable);
+    console.log(notANumber + ' is from type ' + typeof notANumber);
+    console.log(func.name + ' is from type ' + typeof func);
 
     // Task 4
     // Create null, undefined variables and try typeof on them.
 
     var empty = null,
         notExisting = undefined;
-    console.log(empty + ' is from type ' + typeof(empty));
-    console.log(notExisting + ' is from type ' + typeof(notExisting));
+    console.log(empty + ' is from type ' + typeof empty);
+    console.log(notExisting + ' is from type ' + typeof notExisting);
 })();
 
 
