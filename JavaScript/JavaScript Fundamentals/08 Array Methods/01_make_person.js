b8759a6057ee0f2278767f1bd86c5bbe6d753ff8
ivossss <ivossss@yahoo.com>
2015-06-09 13:10:15 +0300

    people of age upload

--- JavaScript/JavaScript Fundamentals/08 Array Methods/01_make_person.js
+++ JavaScript/JavaScript Fundamentals/08 Array Methods/01_make_person.js
@@ -1,3 +1,7 @@
+// Write a function for creating persons.
+// Each person must have firstname, lastname, age and gender (true is female, false is male)
+// Generate an array with ten person with different names, ages and genders
+
 (function () {
     'use strict';
     if (!Array.prototype.fill) {
---
+++ JavaScript/JavaScript Fundamentals/08 Array Methods/02_people_of_age.js
@@ -0,0 +1,30 @@
+// Write a function that checks if an array of person contains only people of age (with age 18 or greater)
+// Use only array methods and no regular loops (for, while)
+
+(function () {
+    'use strict';
+    function Person(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    Person.prototype.toString = function () {
+        return this.firstName + ' ' + this.lastName + ' at ' + this.age;
+    };
+
+    var people = [
+        new Person('Gosho', 'Petrov', 23),
+        new Person('Ivan', 'Topalov', 12),
+        new Person('John', 'Smith', 53),
+        new Person('Monika', 'Georgieva', 10),
+        new Person('Antonia', 'Arsova', 28)
+    ];
+
+    people.filter(function (item) {
+        return item.age >= 18;
+    }).forEach(function (item) {
+        console.log(item.toString());
+    });
+
+})();
