6ca0b7cf5ad067ab3fe699af02c1f4647577666e
ivossss <ivossss@yahoo.com>
2015-07-08 13:41:41 +0300

    Naming Identifiers task 3 refactoring

---
+++ C#/High Quality Code/02 Naming Identifiers/NamingIdentifiers.Task3/JavaScript1.js
@@ -0,0 +1,10 @@
+﻿function onButtonClick(event, arguments) {
+    var browserName = window.navigator.appCodeName,
+        isMozilla = browserName === "Mozilla";
+
+    if (isMozilla) {
+        alert("Yes");
+    } else {
+        alert("No");
+    }
+}
