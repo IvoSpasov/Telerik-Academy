dace74f4093251c278b091ea74f26cde6a9865c6
Ivo Spasov <ivossss@yahoo.com>
2015-11-01 22:47:48 +0200

    CoursesController added.

---
+++ Software technologies/Web Services and Cloud/01 ASP.NET Web API/StudentSystem/StudentSystem.Services/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿namespace StudentSystem.Services.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+
+    using Data.Repositories;
+    using Models;
+    using Data;
+
+    public class CoursesController : ApiController
+    {
+        private IGenericRepository<Course> coursesRepository;
+
+        public CoursesController()
+            : this(new GenericRepository<Course>(new StudentSystemDbContext()))
+        {
+        }
+
+        public CoursesController(IGenericRepository<Course> coursesGenericRepository)
+        {
+            this.coursesRepository = coursesGenericRepository;
+        }
+
+        public IHttpActionResult Get()
+        {
+            var courses = this.coursesRepository.All().ToList();
+            return this.Ok(courses);                
+        }
+    }
+}
