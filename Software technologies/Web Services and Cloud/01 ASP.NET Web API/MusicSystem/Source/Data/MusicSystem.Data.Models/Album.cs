ab167c6321a2148ff1507f50845b10f29f9298a5
Ivo Spasov <ivossss@yahoo.com>
2015-11-06 15:35:45 +0200

    Database models added

---
+++ Software technologies/Web Services and Cloud/01 ASP.NET Web API/MusicSystem/Source/Data/MusicSystem.Data.Models/Album.cs
@@ -0,0 +1,36 @@
+﻿namespace MusicSystem.Data.Models
+{
+    using System.Collections.Generic;
+
+    public class Album
+    {
+        private ICollection<Artist> artists;
+        private ICollection<Song> songs;
+
+        public Album()
+        {
+            this.Artists = new HashSet<Artist>();
+            this.Songs = new HashSet<Song>();
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Year { get; set; }
+
+        public string Producer { get; set; }
+
+        public virtual ICollection<Artist> Artists
+        {
+            get { return this.artists; }
+            set { this.artists = value; }
+        }
+
+        public virtual ICollection<Song> Songs
+        {
+            get { return this.songs; }
+            set { this.songs = value; }
+        }
+    }
+}
---
+++ Software technologies/Web Services and Cloud/01 ASP.NET Web API/MusicSystem/Source/Data/MusicSystem.Data.Models/Artist.cs
@@ -0,0 +1,33 @@
+﻿namespace MusicSystem.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Artist
+    {
+        private ICollection<Song> songs;
+
+        public Artist()
+        {
+            this.Songs = new HashSet<Song>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public virtual Album Album { get; set; }
+
+        public virtual ICollection<Song> Songs
+        {
+            get { return this.songs; }
+            set { this.songs = value; }
+        }
+    }
+}
