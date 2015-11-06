f252ed951f0f7829ac9b5f568e8ad4c99612b318
Ivo Spasov <ivossss@yahoo.com>
2015-11-06 15:13:33 +0200

    MusicSystem skeleton created for Web Api.

---
+++ Software technologies/Web Services and Cloud/01 ASP.NET Web API/MusicSystem/Server/MusicSystem.Server.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MusicSystem.Server.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Software technologies/Web Services and Cloud/01 ASP.NET Web API/MusicSystem/Server/MusicSystem.Server.Api/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace MusicSystem.Server.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
