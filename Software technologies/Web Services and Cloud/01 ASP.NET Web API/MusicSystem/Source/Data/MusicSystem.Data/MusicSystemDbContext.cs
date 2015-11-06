51afd7317d27fbda62a4f9b44234e6a0cceaa603
Ivo Spasov <ivossss@yahoo.com>
2015-11-06 15:47:44 +0200

    Entity framework installed and MusicSystemDbContext added.

---
+++ Software technologies/Web Services and Cloud/01 ASP.NET Web API/MusicSystem/Source/Data/MusicSystem.Data/MusicSystemDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace MusicSystem.Data
+{
+    using System.Data.Entity;
+
+    using Models;
+
+    public class MusicSystemDbContext : DbContext
+    {
+        public MusicSystemDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public IDbSet<Album> Albums { get; set; }
+
+        public IDbSet<Artist> Artists { get; set; }
+
+        public IDbSet<Song> Songs { get; set; }
+    }
+}
