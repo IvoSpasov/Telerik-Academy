62d8037ea4291a316e954fcb89f2bed777537395
Ivo Spasov <ivossss@yahoo.com>
2015-11-11 22:51:09 +0200

    Added a few mocks

---
+++ Web Services and Cloud/01 ASP.NET Web API/MusicSystem/Tests/MusicSystem.Tests.Server.Api/ControllerTests/SongsControllerTests.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicSystem.Tests.Server.Api.ControllerTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Services.Data.Interfaces;
+
+    [TestClass]
+    public class SongsControllerTests
+    {
+        private ISongsService songsService;
+    }
+}
--- Web Services and Cloud/01 ASP.NET Web API/MusicSystem/Tests/MusicSystem.Tests.Server.Api/TestObjectFactory.cs
+++ Web Services and Cloud/01 ASP.NET Web API/MusicSystem/Tests/MusicSystem.Tests.Server.Api/TestObjectFactory.cs
@@ -1,6 +1,35 @@
 ﻿namespace MusicSystem.Tests.Server.Api
 {
-    public class TestObjectFactory
+    using Moq;
+    using Data.Models;
+    using Services.Data.Interfaces;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public static class TestObjectFactory
     {
+        private static IQueryable<Song> songs = new List<Song>()
+        {
+            new Song
+            {
+                Title = "Test title",
+                Year = "Test year",
+                Genre = Genre.Rock                
+            }
+        }.AsQueryable();
+
+        public static ISongsService GetSongsService()
+        {
+            var songsService = new Mock<ISongsService>();
+            songsService.Setup(s => s.All())
+                .Returns(songs);
+            //songsService.Setup(s => s.SongById(It.Is<int>(id => id == 1)))
+            //    .Returns(songs.FirstOrDefault());
+            //songsService.Setup(s => s.SongById(It.Is<int>(id => id == 2)))
+            //    .Returns(null);
+
+
+            return songsService.Object;
+        }
     }
 }
