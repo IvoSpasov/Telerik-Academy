e49a92a4ab4bfabfe889d2b57025bd5e6974ba56
Ivo Spasov <ivossss@yahoo.com>
2015-11-11 12:35:41 +0200

    TestObjectFactory class added for returning repositories

---
+++ Web Services and Cloud/01 ASP.NET Web API/MusicSystem/Tests/MusicSystem.Tests.Services.Data/TestObjects/TestObjectFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace MusicSystem.Tests.Services.Data.TestObjects
+{
+    using MusicSystem.Data.Models;
+
+    public static class TestObjectFactory
+    {
+        public static InMemoryRepository<Song> GetSongsRepository()
+        {
+            var songsRepository = new InMemoryRepository<Song>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                songsRepository.Add(new Song()
+                {
+                    Id = i,
+                    Title = "Song title " + i,
+                    Genre = Genre.Rock,
+                    Year = (1980 + i).ToString()
+                });
+            }
+
+            return songsRepository;
+        }
+    }
+}
