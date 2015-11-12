a161698dd38efb6f99c3fa94f9d20d562d919cfc
Ivo Spasov <ivossss@yahoo.com>
2015-11-12 16:40:18 +0200

    Test projects added.

---
+++ Web Services and Cloud/07 Exam Template/Exam/Tests/Exam.Tests.Server.Api/TestInit.cs
@@ -0,0 +1,25 @@
+﻿namespace Exam.Tests.Server.Api
+{
+    using System.Reflection;
+    using System.Web.Http;
+
+    using Common.Constants;
+    using Exam.Server.Api;
+    using Exam.Server.Infrastructure.Mapping;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MyTested.WebApi;
+
+    [TestClass]
+    public class TestInit
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            AutoMapperConfig.RegisterMappings(Assembly.Load(Assemblies.ServerApi));
+
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            MyWebApi.IsUsing(config);
+        }
+    }
+}
---
+++ Web Services and Cloud/07 Exam Template/Exam/Tests/Exam.Tests.Services.Data/TestObjects/TestObjectFactory.cs
@@ -0,0 +1,27 @@
+﻿namespace Exam.Tests.Services.Data.TestObjects
+{
+    using System;
+
+    using Exam.Data.Models;
+
+    public static class TestObjectFactory
+    {
+        //public static InMemoryRepository<Song> GetSongsRepository()
+        //{
+        //    var songsRepository = new InMemoryRepository<Song>();
+
+        //    for (int i = 0; i < 25; i++)
+        //    {
+        //        songsRepository.Add(new Song()
+        //        {
+        //            Id = i,
+        //            Title = "Song title " + i,
+        //            Genre = Genre.Rock,
+        //            Year = (1980 + i).ToString()
+        //        });
+        //    }
+
+        //    return songsRepository;
+        //}
+    }
+}
