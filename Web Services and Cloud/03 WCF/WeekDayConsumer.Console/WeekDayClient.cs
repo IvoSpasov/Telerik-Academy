40ca7995ea719431d9f83daf92ca2a87aa1b74cd
Ivo Spasov <ivossss@yahoo.com>
2015-11-13 16:32:26 +0200

    WCF homework task 2 upload

---
+++ Web Services and Cloud/03 WCF/WeekDayConsumer.Console/WeekDayClient.cs
@@ -0,0 +1,16 @@
+﻿namespace WeekDayConsumer.Console
+{
+    using ServiceWeekDay;
+    using System;
+
+    class WeekDayClient
+    {
+        static void Main()
+        {
+            ServiceWeekDayClient client = new ServiceWeekDayClient();
+            var date = new DateTime(2015, 11, 13);
+            string dayOfWeek = client.GetWeekDay(date);
+            Console.WriteLine("The day of the week on {0} is {1}", date.ToShortDateString(), dayOfWeek);
+        }
+    }
+}
