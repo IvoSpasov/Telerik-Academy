9b8183d158505159e434b3f194ccc9559931d1cb
Ivo Spasov <ivossss@yahoo.com>
2015-11-13 17:10:20 +0200

    WCF hw task 3 upload

---
+++ Web Services and Cloud/03 WCF/WcfServiceWeekDay.Library/ServiceStringCount.cs
@@ -0,0 +1,24 @@
+﻿namespace WcfServiceWeekDay.Library
+{
+    using System;
+
+    public class ServiceStringCount : IServiceStringCount
+    {
+        public int StringCount(string text, string searchedWord)
+        {
+            string[] separators = { ".", " ", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+            string[] wordsArray = text.ToLower().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int counter = 0;
+
+            foreach (var word in wordsArray)
+            {
+                if (word == searchedWord.ToLower())
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
