2c309dc693ca258c8be60bf7644d4b179efca6f3
Ivo Spasov <ivossss@yahoo.com>
2015-11-13 16:17:09 +0200

    WCF homework task 1 upload

---
+++ Web Services and Cloud/03 WCF/WcfServiceWeekDay.Web/IServiceWeekDay.cs
@@ -0,0 +1,12 @@
+﻿namespace WcfServiceWeekDay.Web
+{
+    using System;
+    using System.ServiceModel;
+
+    [ServiceContract]
+    public interface IServiceWeekDay
+    {
+        [OperationContract]
+        string GetWeekDay(DateTime date);
+    }
+}
---
+++ Web Services and Cloud/03 WCF/WcfServiceWeekDay.Web/ServiceWeekDay.svc.cs
@@ -0,0 +1,32 @@
+﻿namespace WcfServiceWeekDay.Web
+{
+    using System;
+
+    public class ServiceWeekDay : IServiceWeekDay
+    {
+        public string GetWeekDay(DateTime date)
+        {
+            var weekDay = date.DayOfWeek;
+
+            switch (weekDay)
+            {
+                case DayOfWeek.Sunday:
+                    return "Неделя";
+                case DayOfWeek.Monday:
+                    return "Понеделник";
+                case DayOfWeek.Tuesday:
+                    return "Вторник";
+                case DayOfWeek.Wednesday:
+                    return "Сряда";
+                case DayOfWeek.Thursday:
+                    return "Четвъртък";
+                case DayOfWeek.Friday:
+                    return "Петък";
+                case DayOfWeek.Saturday:
+                    return "Събота";
+                default:
+                    return "Invalid day";
+            }
+        }
+    }
+}
